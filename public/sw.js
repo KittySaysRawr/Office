2025e63c951f87af743cb04ad30a212603d3aa53
GreenMeans <pastul12345@live.co.uk>
2026-06-10 14:27:50 +0100

    Modify asset paths in service worker
    Updated asset paths in service worker to include 'public/' prefix.

--- public/sw.js
+++ public/sw.js
@@ -2,15 +2,15 @@ const CACHE_NAME = "greenmeans-office-v1";
 const ASSETS = [
   "/",
   "/index.html",
-  "/manifest.json",
-  "/favicon.ico",
-  "/favicon-16x16.png",
-  "/favicon-32x32.png",
-  "/favicon-round.svg",
-  "/favicon-square.svg",
-  "/android-chrome-192x192.png",
-  "/android-chrome-512x512.png",
-  "/apple-touch-icon.png"
+  "/public/manifest.json",
+  "/public/favicon.ico",
+  "/public/favicon-16x16.png",
+  "/public/favicon-32x32.png",
+  "/public/favicon-round.svg",
+  "/public/favicon-square.svg",
+  "/public/android-chrome-192x192.png",
+  "/public/android-chrome-512x512.png",
+  "/public/apple-touch-icon.png"
 ];
 
 // Install: Cache critical shell assets
